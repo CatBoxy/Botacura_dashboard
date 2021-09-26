33528e702518dcf6a300c66898e1d9635485033f
CatBoxy <thecatbox@protonmail.com>
2021-09-26 18:02:10 -0300

    first commit

---
+++ src/components/ContentRowTop.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import CategoriesInDB from './CategoriesInDB';
+import ContentRowCards from './ContentRowCards';
+import LastProductInDB from './LastProductInDB';
+function ContentRowTop(){
+    return(
+        <React.Fragment>
+				{/*<!-- Content Row Top -->*/}
+				<div className="container-fluid">
+					<div className="d-sm-flex aligns-items-center justify-content-between mb-4">
+						<h1 className="h3 mb-0 text-gray-800">App Dashboard</h1>
+					</div>
+				
+					{/*<!-- Content Row Movies-->*/}
+					<ContentRowCards />
+					{/*<!-- End movies in Data Base -->*/}
+					
+	
+					{/*<!-- Content Row Last Movie in Data Base -->*/}
+					<div className="row">
+						{/*<!-- Last Movie in DB -->*/}
+						<LastProductInDB />
+						{/*<!-- End content row last movie in Data Base -->*/}
+
+						{/*<!-- Genres in DB -->*/}
+						<CategoriesInDB />
+
+						{/*<!--End Genres In Db-->*/}		
+					</div>
+				</div>
+				{/*<!--End Content Row Top-->*/}
+
+        </React.Fragment>
+    )
+
+}
+export default ContentRowTop;
